23492b86e61d4b14cc5bf7f534e5f8737f10c842
Bruno Pacheco <brunopacheco1@yahoo.com>
2016-11-08 14:54:00 +0000

    CORS.

--- service-sms/src/com/dev/bruno/servicesms/resources/BaseApplication.java
+++ service-sms/src/com/dev/bruno/servicesms/resources/BaseApplication.java
@@ -1,12 +1,10 @@
 package com.dev.bruno.servicesms.resources;
 
-import java.util.HashSet;
-import java.util.Set;
+import io.swagger.jaxrs.config.BeanConfig;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import io.swagger.jaxrs.config.BeanConfig;
 
 @ApplicationPath("api")
 public class BaseApplication extends Application {
@@ -19,15 +17,5 @@ public class BaseApplication extends Application {
         beanConfig.setBasePath("/api");
         beanConfig.setResourcePackage("com.dev.bruno.servicesms.resources");
         beanConfig.setScan(true);
-	}
-
-	@Override
-	public Set<Class<?>> getClasses() {
-		Set<Class<?>> resources = new HashSet<>();
-
-        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
-        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
-
-        return resources;
 	}
 }
