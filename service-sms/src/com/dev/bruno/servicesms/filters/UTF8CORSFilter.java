c2082264bcb4b3803a91cc3d8dee8d9cd97967d7
Bruno Pacheco <brunopacheco1@yahoo.com>
2016-11-08 14:55:32 +0000

    CORS de verdade agora.

---
+++ service-sms/src/com/dev/bruno/servicesms/filters/UTF8CORSFilter.java
@@ -0,0 +1,26 @@
+package com.dev.bruno.servicesms.filters;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class UTF8CORSFilter implements ContainerResponseFilter {
+    
+    @Override
+ 	public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
+ 		MediaType contentType = responseContext.getMediaType();
+ 		if (contentType != null && !contentType.toString().contains("charset")) {
+            responseContext.getHeaders().putSingle("Content-Type", contentType + ";charset=utf-8");
+        }
+        
+        responseContext.getHeaders().putSingle("Access-Control-Expose-Headers", "Content-Location");
+	    responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+	    responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
+	    responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept-Encoding, USER, NroMatricula, CHAVEREQUISICAO");
+    }
+}
