5709e5fd578556f9881f954b3743df263de63253
Bruno Pacheco <brunopacheco1@yahoo.com>
2016-11-10 00:14:10 -0200

    Refatoração do projeto para receber estrutura de casos de testes.

---
+++ service-sms/src/test/java/com/dev/bruno/servicesms/test/OperadoraTest.java
@@ -0,0 +1,45 @@
+package com.dev.bruno.servicesms.test;
+
+import javax.ejb.embeddable.EJBContainer;
+
+import org.junit.Test;
+
+import com.dev.bruno.servicesms.service.OperadoraFactoryService;
+
+import junit.framework.TestCase;
+
+public class OperadoraTest extends TestCase {
+
+	private EJBContainer ejbContainer;
+
+	private OperadoraFactoryService service;
+
+	@Override
+	protected void setUp() throws Exception {
+		ejbContainer = EJBContainer.createEJBContainer();
+		Object object = ejbContainer.getContext().lookup("java:global/service-sms/OperadoraFactoryService");
+		
+		assertTrue(object instanceof OperadoraFactoryService);
+
+		service = (OperadoraFactoryService) object;
+	}
+
+	protected void tearDown() throws Exception {
+		ejbContainer.close();
+	}
+
+	@Test
+	public void testNullOperadora() {
+		assertNull(service.getOperadora(null));
+	}
+	
+	@Test
+	public void testDestinatarioNoMatch() {
+		assertNull(service.getOperadora("2222"));
+	}
+	
+	@Test
+	public void testDestinatarioMatch() {
+		assertNotNull(service.getOperadora("+5521999112222"));
+	}
+}
